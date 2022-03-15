bada17e6f4421c09dd615f5b659c8976320363aa
WilliamTRev <92125097+WilliamTRev@users.noreply.github.com>
2022-03-15 11:58:54 -0400

    updated signle node doc

--- Week2/JDBCEasyScala/src/main/scala/com/Revature/JDBC1.scala
+++ Week2/JDBCEasyScala/src/main/scala/com/Revature/JDBC1.scala
@@ -15,7 +15,7 @@ object JDBC1 {
     val username = "root"
     val password = "p4ssword"
 
-    var connection:Connection = DriverManager.getConnection(url, username, password)
+    val connection:Connection = DriverManager.getConnection(url, username, password)
     val statement = connection.createStatement()
     val resultSet = statement.executeQuery("SELECT * FROM users;")
 
@@ -29,7 +29,7 @@ object JDBC1 {
     //Class.forName(driver)
     //val connection2 = DriverManager.getConnection(url, username, password)
     val statement2=connection.createStatement()
-    statement2.executeUpdate("INSERT INTO users VALUES(6, 'start', '2000-01-06');")
+    statement2.executeUpdate("INSERT INTO users VALUES(7, 'start', '2000-02-08');")
 
     connection.close()
   }
